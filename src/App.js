acc656242c93907d0afd5aee23f11faf11799060
eternale88 <ethanrobbins@ymail.com>
2019-10-07 16:04:46 -0400

    add components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//App.js
+import React, { Component } from 'react'
+import DateInput from './components/DateInput'
+import Photo from './components/Photo.js'
+import { API_KEY } from './keys.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    date: '',
+    photo: ''
+  }
+  changeDate = (e) => {
+    e.preventDefault()
+    let dateFromInput = e.target[0].value
+    this.setState({ date: dateFromInput })
+    console.log
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>NASA's Astronomy Picture of the Day</h1>
+        <DateInput changeDate={this.changeDate} />
+        <Photo />
+      </div>
+    )
+  }
 }
-
-export default App;
+export default App
---
+++ src/components/Photo.js
@@ -0,0 +1,3 @@
+import React from 'react'
+const Photo = (props) => <div>Photo</div>
+export default Photo
