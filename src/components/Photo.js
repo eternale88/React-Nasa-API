3173d68a1745f6747169c2a21333c443f437f94f
eternale88 <ethanrobbins@ymail.com>
2019-10-10 13:54:05 -0400

    add conditional to check if is video or photo to render correct HTML

--- src/components/Photo.js
+++ src/components/Photo.js
@@ -1,20 +1,39 @@
 import React from 'react'
-const Photo = (props) => (
-  <div className="card card-body bg-light">
-    <h4 className="text-center">{props.photo.title}</h4>
-    {
-      // <iframe
-      //   className="img-fluid rounded"
-      //   src={props.photo.url}
-      //   alt={props.photo.title}
-      // ></iframe>
+const Photo = (props) => {
+  //check to see if nasa api returns video or image
+  // and render correct html accordingly
+  function renderContentType() {
+    if (props.photo.media_type === 'image') {
+      return (
+        <img
+          style={{ marginBottom: '20px' }}
+          className="img-fluid rounded"
+          src={props.photo.url}
+          alt={props.photo.title}
+        />
+      )
+    } else if (props.photo.media_type === 'video') {
+      return (
+        <iframe
+          style={{ marginBottom: '20px' }}
+          title="nasa video of the day"
+          className="img-fluid rounded"
+          src={props.photo.url}
+          alt={props.photo.title}
+        ></iframe>
+      )
+    } else {
+      return
     }
-    <img
-      className="img-fluid rounded"
-      src={props.photo.url}
-      alt={props.photo.title}
-    />
-    <p>{props.photo.explanation}</p>
-  </div>
-)
+  }
+  return (
+    <div className="card card-body bg-light">
+      <h4 className="text-center">{props.photo.title}</h4>
+
+      {renderContentType()}
+      <p>{props.photo.explanation}</p>
+    </div>
+  )
+}
+
 export default Photo
